bff291603de0887242784599169ea88d727f9d76
YonePC <YonePC@YONE>
2018-01-19 11:21:41 +0000

    Done an asynchronous call in the beforeEach to be able to load the template and style of BannerComponent from external files before initializing test suite.

---
+++ src/app/banner.component.spec.ts
@@ -0,0 +1,53 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By }              from '@angular/platform-browser';
+import { DebugElement }    from '@angular/core';
+
+import { BannerComponent } from './banner.component';
+
+
+
+describe('BannerComponent (templateUrl)', () => {
+
+  let comp:    BannerComponent;
+  let fixture: ComponentFixture<BannerComponent>;
+  let de:      DebugElement;
+  let el:      HTMLElement;
+
+  configureTestToMakeAutomaticChangeDetectionAfterAngularCompilerLoadsTemplatesAndStyles();
+
+  // synchronous beforeEach
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BannerComponent);
+
+    comp = fixture.componentInstance; // BannerComponent test instance
+
+    // query for the title <h1> by CSS element selector
+    de = fixture.debugElement.query(By.css('h1'));
+    el = de.nativeElement;
+  });
+
+  it('no title in the DOM until manually call `detectChanges`', () => {
+    expect(el.textContent).toEqual('');
+  });
+
+  it('should display original title', () => {
+    fixture.detectChanges();
+    expect(el.textContent).toContain(comp.title);
+  });
+
+  it('should display a different test title', () => {
+    comp.title = 'Test Title';
+    fixture.detectChanges();
+    expect(el.textContent).toContain('Test Title');
+  });
+
+});
+
+function configureTestToMakeAutomaticChangeDetectionAfterAngularCompilerLoadsTemplatesAndStyles() {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [BannerComponent],
+    })
+      .compileComponents();
+  }));
+}
